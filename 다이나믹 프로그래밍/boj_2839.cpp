ea279c14f6e535f55bf4fdc19be2165f0809c515
Sp.arc <pcpark98@naver.com>
2022-11-11 21:16:07 +0900

    solved: 22-11-11

---
+++ 다이나믹 프로그래밍/boj_2839.cpp
@@ -0,0 +1,19 @@
+// 백준 2839번 설탕 배달
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> dp = { 0, 5000,5000,1,5000,1 };
+int N;
+
+int main() {
+	cin >> N;
+	while (dp.size() <= N) {
+		int newValue = min(dp[dp.size() - 3], dp[dp.size() - 5]);
+		if (newValue == 5000) dp.push_back(5000);
+		else dp.push_back(newValue + 1);
+	}
+	if (dp[N] == 5000) cout << -1;
+	else cout << dp[N];
+}
